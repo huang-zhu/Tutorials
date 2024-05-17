0afb89d891afc2022c36216b198fa47f59b2df4b
huang-zhu <alberto_h.14@hotmail.com>
2024-05-17 08:29:17 -0500

    Removing test files from CG_WaterBox.

---
+++ WaterBox/run_em.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### DEFINE BINARIES
+GMX="$(which gmx)"
+
+### PREPARE EM DIRECTORY
+CURRENT=em
+mkdir -p ${CURRENT} 
+
+### GENERATE TPR
+${GMX} grompp -f input_files/em.mdp \
+              -c input_files/init.gro \
+              -p input_files/topology.top \
+              -po ${CURRENT}/mdout_${CURRENT}.mdp \
+              -o ${CURRENT}/${CURRENT}.tpr \
+              -maxwarn 1 
+
+### RUN EM
+${GMX} mdrun -v -deffnm ${CURRENT}/${CURRENT}
+
+### ANALYZE
+${GMX} energy -f ${CURRENT}/${CURRENT}.edr \
+              -o ${CURRENT}/energy.xvg <<< $'Potential'  
+
+### CLEAN-UP
+rm ${CURRENT}/#*
